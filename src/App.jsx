1d153a10f139bbcc5d92bddea04669501b4b86af
kyzw <kota.yazawa@super-studio.jp>
2022-09-27 21:34:02 +0000

    develop CRUD function

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,42 @@ import "./styles.css";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setInconmpleteTodos] = useState(["ABC", "DEF"]);
-  const [completeTodos, setConmpleteTodos] = useState(["GHI", "JKL"]);
+  const [incompleteTodos, setInconmpleteTodos] = useState([]);
+  const [completeTodos, setConmpleteTodos] = useState([]);
+
   const onChangeTodoText = (event) => setTodoText(event.target.value);
+
   const onClickAdd = () => {
     if (todoText === "") return;
     const newTodos = [...incompleteTodos, todoText];
     setInconmpleteTodos(newTodos);
     setTodoText("");
   };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setInconmpleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setInconmpleteTodos(newIncompleteTodos);
+    setConmpleteTodos(newCompleteTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setConmpleteTodos(newCompleteTodos);
+    setInconmpleteTodos(newIncompleteTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -26,12 +53,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">Uncompleted TODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key="{todo}" className="list-row">
                 <li>{todo}</li>
-                <button>Done</button>
-                <button>Delete</button>
+                <button onClick={() => onClickComplete(index)}>Done</button>
+                <button onClick={() => onClickDelete(index)}>Delete</button>
               </div>
             );
           })}
@@ -40,11 +67,11 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">Completed TODO</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <div key="{todo}" className="list-row">
                 <li>{todo}</li>
-                <button>Back</button>
+                <button onClick={() => onClickBack(index)}>Back</button>
               </div>
             );
           })}
